0fd0b9607134d54adb481f8a69712d3683542aa8
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-10-11 11:04:07 -0500

    adds circle to header

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -4,9 +4,20 @@ import { PiCaretDown } from "react-icons/pi";
 
 const Header = ({ documentType, user }: HeaderProps) => {
   return (
-    <header className="flex justify-between items-center h-16 px-6 bg-white border-b-[3px] border-gray-200">
+    <header className="relative flex justify-between items-center h-16 px-6 bg-white border-b-[3px] border-gray-200">
+      <div
+        className="absolute bg-white border border-black w-[35px] h-[35px] rounded-full flex items-center justify-center"
+        style={{
+          left: "-20px",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      >
+        <span className="text-black text-lg">&lt;&gt;</span>{" "}
+        {/* Centered content */}
+      </div>
       <div className="flex items-center min-h-[30px]">
-        <h1 className="text-lg font-bold ">{documentType}</h1>
+        <h1 className="text-lg font-bold">{documentType}</h1>
       </div>
       <div className="flex items-center gap-5">
         <FaRegBell className="text-lg" />
